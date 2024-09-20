490b5df8128159a704cb4bd856d7575a9a8c1f66
kauatwn <kaualopescontato@gmail.com>
2024-09-20 19:04:10 -0300

    Criando PayPalPaymentService

---
+++ src/Infrastructure/Services/Payments/PayPalPaymentService.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Interfaces;
+
+namespace Infrastructure.Services.Payments;
+
+public class PayPalPaymentService : IPayment
+{
+    public string Pay(double amount)
+    {
+        return $"Processing PayPal payment with amount: {amount}";
+    }
+}
