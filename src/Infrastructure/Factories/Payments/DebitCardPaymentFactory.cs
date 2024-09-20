4157c8fe8814143dc052256db14ba7978fede1db
kauatwn <kaualopescontato@gmail.com>
2024-09-20 19:08:41 -0300

    Criando classe concreta DebitCardPaymentFactory

---
+++ src/Infrastructure/Factories/Payments/DebitCardPaymentFactory.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Interfaces;
+using Infrastructure.Factories.Payments.Base;
+using Infrastructure.Services.Payments;
+
+namespace Infrastructure.Factories.Payments;
+
+public class DebitCardPaymentFactory : PaymentFactory
+{
+    public override IPayment Create()
+    {
+        return new DebitCardPaymentService();
+    }
+}
