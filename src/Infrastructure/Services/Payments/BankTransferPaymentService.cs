f61a8b4929ce05ff62762482733662223e22e5e1
kauatwn <kaualopescontato@gmail.com>
2024-09-20 19:03:45 -0300

    Criando BankTransferPaymentService

---
+++ src/Infrastructure/Services/Payments/BankTransferPaymentService.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Interfaces;
+
+namespace Infrastructure.Services.Payments;
+
+public class BankTransferPaymentService : IPayment
+{
+    public string Pay(double amount)
+    {
+        return $"Processing bank transfer payment with amount: {amount}";
+    }
+}
