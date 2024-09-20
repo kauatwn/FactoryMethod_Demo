8bd2b39f8acbcc4e600e4db5324a46738d899c9a
kauatwn <kaualopescontato@gmail.com>
2024-09-20 19:11:43 -0300

    Criando DependencyInjection

---
+++ src/CrossCutting/IoC/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using Application.Abstractions.Factories;
+using Application.Abstractions.UseCases;
+using Application.UseCases;
+using Domain.Enums;
+using Infrastructure.Factories.Payments;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CrossCutting.IoC;
+
+public static class DependencyInjection
+{
+    public static void AddApplication(this IServiceCollection services)
+    {
+        AddFactories(services);
+        AddUseCases(services);
+    }
+
+    private static void AddFactories(IServiceCollection services)
+    {
+        services.AddKeyedSingleton<IPaymentFactory, BankTransferPaymentFactory>(PaymentMethod.BankTransfer);
+        services.AddKeyedSingleton<IPaymentFactory, CreditCardPaymentFactory>(PaymentMethod.CreditCard);
+        services.AddKeyedSingleton<IPaymentFactory, DebitCardPaymentFactory>(PaymentMethod.DebitCard);
+        services.AddKeyedSingleton<IPaymentFactory, PayPalPaymentFactory>(PaymentMethod.PayPal);
+    }
+
+    private static void AddUseCases(IServiceCollection services)
+    {
+        services.AddScoped<IProcessPaymentUseCase, ProcessPaymentUseCase>();
+    }
+}
