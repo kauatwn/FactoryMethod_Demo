e4941aafbfa80356fa9ac98f67e61b40cf9e28a1
kauatwn <kaualopescontato@gmail.com>
2024-09-20 19:04:02 -0300

    Criando DebitCardPaymentService

---
+++ src/Infrastructure/Services/Payments/DebitCardPaymentService.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Interfaces;
+
+namespace Infrastructure.Services.Payments;
+
+public class DebitCardPaymentService : IPayment
+{
+    public string Pay(double amount)
+    {
+        return $"Processing debit card payment with amount: {amount}";
+    }
+}
