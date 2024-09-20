1e0b3af05126d1fa60842b8c123f08588a83cfe9
kauatwn <kaualopescontato@gmail.com>
2024-09-20 19:08:29 -0300

    Criando classe concreta PayPalPaymentFactory

---
+++ src/Infrastructure/Factories/Payments/CreditCardPaymentFactory.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Interfaces;
+using Infrastructure.Factories.Payments.Base;
+using Infrastructure.Services.Payments;
+
+namespace Infrastructure.Factories.Payments;
+
+public class CreditCardPaymentFactory : PaymentFactory
+{
+    public override IPayment Create()
+    {
+        return new CreditCardPaymentService();
+    }
+}
