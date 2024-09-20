bd0e8193e3455340570256df43dbe96e52c8be77
kauatwn <kaualopescontato@gmail.com>
2024-09-20 19:03:14 -0300

    Criando classe base PaymentFactory

---
+++ src/Infrastructure/Factories/Payments/Base/PaymentFactory.cs
@@ -0,0 +1,9 @@
+﻿using Application.Abstractions.Factories;
+using Domain.Interfaces;
+
+namespace Infrastructure.Factories.Payments.Base;
+
+public abstract class PaymentFactory : IPaymentFactory
+{
+    public abstract IPayment Create();
+}
