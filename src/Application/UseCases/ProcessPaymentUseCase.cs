780e02f3760c65747a3b440dbe78e43964f2b496
kauatwn <kaualopescontato@gmail.com>
2024-09-20 18:49:54 -0300

    Criando ProcessPaymentUseCase

---
+++ src/Application/UseCases/ProcessPaymentUseCase.cs
@@ -0,0 +1,19 @@
+﻿using Application.Abstractions.Factories;
+using Application.Abstractions.UseCases;
+using Domain.Entities;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.UseCases;
+
+public class ProcessPaymentUseCase(IServiceProvider serviceProvider) : IProcessPaymentUseCase
+{
+    private IServiceProvider ServiceProvider { get; } = serviceProvider;
+
+    public string Execute(Payment payment)
+    {
+        var factory = ServiceProvider.GetRequiredKeyedService<IPaymentFactory>(payment.Method);
+
+        var process = factory.Create();
+        return process.Pay(payment.Amount);
+    }
+}
