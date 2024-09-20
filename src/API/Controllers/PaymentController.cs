21b73881050bdca5b243400a176f7bf94f7c9603
kauatwn <kaualopescontato@gmail.com>
2024-09-20 19:14:00 -0300

    Criando PaymentController

---
+++ src/API/Controllers/PaymentController.cs
@@ -0,0 +1,19 @@
+﻿using Application.Abstractions.UseCases;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PaymentController(IProcessPaymentUseCase useCase) : ControllerBase
+{
+    private IProcessPaymentUseCase UseCase { get; } = useCase;
+
+    [HttpPost]
+    public IActionResult ProcessPayment(Payment payment)
+    {
+        var result = UseCase.Execute(payment);
+        return Ok(result);
+    }
+}
