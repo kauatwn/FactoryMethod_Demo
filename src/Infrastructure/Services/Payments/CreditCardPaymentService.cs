2bfce1746be941aa1cde54f3a656da21fe41a588
kauatwn <kaualopescontato@gmail.com>
2024-09-20 19:03:53 -0300

    Criando CreditCardPaymentService

---
+++ src/Infrastructure/Services/Payments/CreditCardPaymentService.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Interfaces;
+
+namespace Infrastructure.Services.Payments;
+
+public class CreditCardPaymentService : IPayment
+{
+    public string Pay(double amount)
+    {
+        return $"Processing credit card payment with amount: {amount}";
+    }
+}
