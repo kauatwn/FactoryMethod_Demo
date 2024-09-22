284f87f5f54306bd38e5551affb9e9873c2dabd2
kauatwn <kaualopescontato@gmail.com>
2024-09-22 15:48:17 -0300

    Adicionando configuração de infraestrutura

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
 
 var app = builder.Build();
 
