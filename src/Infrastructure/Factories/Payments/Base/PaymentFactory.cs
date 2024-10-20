dffd5a3fc11979c71f43dbbd5a758ad21fb1ed73
kauatwn <kaualopescontato@gmail.com>
2024-10-20 19:57:52 -0300

    Removendo classe abstrata PaymentFactory

--- src/Infrastructure/Factories/Payments/Base/PaymentFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using Domain.Interfaces.Factories;
-using Domain.Interfaces.Services;
-
-namespace Infrastructure.Factories.Payments.Base;
-
-public abstract class PaymentFactory : IPaymentFactory
-{
-    public abstract IPaymentService Create();
-}
