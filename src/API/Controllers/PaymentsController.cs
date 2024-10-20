6f847204cf7ae93299d97a1e04641a5e6f65c798
kauatwn <kaualopescontato@gmail.com>
2024-10-20 20:01:34 -0300

    Adicionando ProducesResponseType

--- src/API/Controllers/PaymentsController.cs
+++ src/API/Controllers/PaymentsController.cs
@@ -9,6 +9,7 @@ namespace API.Controllers;
 public class PaymentsController : ControllerBase
 {
     [HttpPost]
+    [ProducesResponseType<Payment>(StatusCodes.Status201Created)]
     public IActionResult ProcessPayment(IProcessPaymentUseCase useCase, Payment payment)
     {
         var result = useCase.Execute(payment);
