31c1e3f5e84d915339dedefcc9b867181fbbb04e
kauatwn <kaualopescontato@gmail.com>
2024-10-23 19:24:25 -0300

    Refatorando ProcessPaymentUseCase para usar mocks

--- test/Application.UnitTests/UseCases/ProcessPaymentUseCaseTest.cs
+++ test/Application.UnitTests/UseCases/ProcessPaymentUseCaseTest.cs
@@ -1,80 +1,59 @@
 using Application.UseCases;
-using CrossCutting.IoC;
 using Domain.Entities;
 using Domain.Enums;
+using Domain.Interfaces.Factories;
+using Domain.Interfaces.Services;
 using Microsoft.Extensions.DependencyInjection;
+using Moq;
 
 namespace Application.UnitTests.UseCases;
 
 public class ProcessPaymentUseCaseTest
 {
-    private IServiceProvider ServiceProvider { get; } = ConfigureServices();
+    private Mock<IKeyedServiceProvider> KeyedServiceProviderMock { get; } = new();
+    private Mock<IPaymentFactory> PaymentFactoryMock { get; } = new();
+    private Mock<IPaymentService> PaymentServiceMock { get; } = new();
     private ProcessPaymentUseCase UseCase { get; }
 
     public ProcessPaymentUseCaseTest()
     {
-        UseCase = new ProcessPaymentUseCase(ServiceProvider);
+        UseCase = new ProcessPaymentUseCase(KeyedServiceProviderMock.Object);
     }
-
-    [Theory]
-    [InlineData(100.0, PaymentMethod.BankTransfer)]
-    [InlineData(200.0, PaymentMethod.CreditCard)]
-    [InlineData(300.0, PaymentMethod.DebitCard)]
-    [InlineData(400.0, PaymentMethod.PayPal)]
-    public void ShouldProcessPaymentWhenPaymentMethodIsSet(double amount, PaymentMethod method)
+    
+    [Fact]
+    public void ShouldProcessPaymentWhenPaymentMethodIsSet()
     {
         // Arrange
-        var payment = new Payment(amount, method);
+        var payment = new Payment(100.0, PaymentMethod.BankTransfer);
 
-        var expectedMessage = method switch
-        {
-            PaymentMethod.BankTransfer => $"Processing bank transfer payment with amount: {amount:C}",
-            PaymentMethod.CreditCard => $"Processing credit card payment with amount: {amount:C}",
-            PaymentMethod.DebitCard => $"Processing debit card payment with amount: {amount:C}",
-            PaymentMethod.PayPal => $"Processing PayPal payment with amount: {amount:C}",
-            _ => throw new InvalidOperationException(
-                "No service for type 'Domain.Interfaces.Factories.IPaymentFactory' has been registered.")
-        };
+        KeyedServiceProviderMock.Setup(ksp =>
+                ksp.GetRequiredKeyedService(typeof(IPaymentFactory), It.IsAny<PaymentMethod>()))
+            .Returns(PaymentFactoryMock.Object);
+
+        PaymentFactoryMock.Setup(f => f.Create()).Returns(PaymentServiceMock.Object);
 
         // Act
-        var result = UseCase.Execute(payment);
+        UseCase.Execute(payment);
 
         // Assert
-        Assert.Equal(expectedMessage, result);
-    }
+        Assert.NotNull(UseCase.PaymentService);
 
+        KeyedServiceProviderMock.Verify(
+            ksp => ksp.GetRequiredKeyedService(typeof(IPaymentFactory), It.IsAny<PaymentMethod>()),
+            Times.Once);
+
+        PaymentFactoryMock.Verify(f => f.Create(), Times.Once);
+        PaymentServiceMock.Verify(s => s.ProcessPayment(It.IsAny<double>()), Times.Once);
+    }
+    
     [Fact]
-    public void ShouldThrowExceptionWhenPaymentMethodIsNotSet()
+    public void ShouldThrowExceptionWhenPaymentMethodIsNotProvidedAndSet()
     {
         // Arrange
         var payment = new Payment(100.0, null);
 
         // Act & Assert
         var exception = Assert.Throws<InvalidOperationException>(() => UseCase.Execute(payment));
-        Assert.Equal("No service for type 'Domain.Interfaces.Factories.IPaymentFactory' has been registered.",
-            exception.Message);
-    }
-
-    [Fact]
-    public void ShouldThrowExceptionWhenPaymentMethodIsInvalid()
-    {
-        // Arrange
-        var invalidPaymentMethod = (PaymentMethod)999;
-        var payment = new Payment(100.0, invalidPaymentMethod);
-
-        // Act & Assert
-        var exception = Assert.Throws<InvalidOperationException>(() => UseCase.Execute(payment));
-        Assert.Equal("No service for type 'Domain.Interfaces.Factories.IPaymentFactory' has been registered.",
-            exception.Message);
-    }
-
-    private static ServiceProvider ConfigureServices()
-    {
-        var services = new ServiceCollection();
-
-        services.AddApplication();
-        services.AddInfrastructure();
-
-        return services.BuildServiceProvider();
+        Assert.Equal("Payment method is not set", exception.Message);
     }
 }
