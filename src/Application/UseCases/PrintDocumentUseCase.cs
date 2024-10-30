3b83df96e49a4fa75e5f8af2e612fce74dc5318e
kauatwn <kaualopescontato@gmail.com>
2024-10-30 15:00:21 -0300

    Refatorando ProcessPaymentUseCase para PrintDocumentUseCase

---
+++ src/Application/UseCases/PrintDocumentUseCase.cs
@@ -0,0 +1,17 @@
+﻿using Application.Abstractions.UseCases;
+using Domain.Entities;
+using Domain.Interfaces.Factories;
+
+namespace Application.UseCases;
+
+public class PrintDocumentUseCase(IDocumentFactory documentFactory) : IPrintDocumentUseCase
+{
+    private IDocumentFactory DocumentFactory { get; } = documentFactory;
+
+    public string Execute(Document document)
+    {
+        var service = DocumentFactory.Create();
+
+        return service.PrintDocument(document);
+    }
+}
