b38183a6998fbc8808d100e7d871dbd512184352
kauatwn <kaualopescontato@gmail.com>
2024-10-30 14:59:03 -0300

    Criando WordDocumentFactory

---
+++ src/Infrastructure/Factories/Documents/WordDocumentFactory.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Interfaces.Factories;
+using Domain.Interfaces.Services;
+using Infrastructure.Services.Documents;
+
+namespace Infrastructure.Factories.Documents;
+
+public class WordDocumentFactory : IDocumentFactory
+{
+    public IDocumentService Create()
+    {
+        return new WordDocumentService();
+    }
+}
