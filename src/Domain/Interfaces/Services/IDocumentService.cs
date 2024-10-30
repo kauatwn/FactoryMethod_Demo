ed499cc75bbfd2684c9aeea0afa9c09002f69982
kauatwn <kaualopescontato@gmail.com>
2024-10-30 14:54:52 -0300

    Refatorando IPaymentService para IDocumentService

---
+++ src/Domain/Interfaces/Services/IDocumentService.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces.Services;
+
+public interface IDocumentService
+{
+    string PrintDocument(Document document);
+}
--- src/Domain/Interfaces/Services/IPaymentService.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Domain.Interfaces.Services;
-
-public interface IPaymentService
-{
-    string ProcessPayment(double amount);
-}
