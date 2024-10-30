1fd0baf25b31bea6f64011d0552936f81e0a4c40
kauatwn <kaualopescontato@gmail.com>
2024-10-30 14:58:01 -0300

    Criando WordDocumentService

---
+++ src/Infrastructure/Services/Documents/WordDocumentService.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using Domain.Interfaces.Services;
+
+namespace Infrastructure.Services.Documents;
+
+public class WordDocumentService : IDocumentService
+{
+    public string PrintDocument(Document document)
+    {
+        return $"Word Document: {document.Title}";
+    }
+}
