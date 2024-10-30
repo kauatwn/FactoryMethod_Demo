9b526c57cf3e666eb01765f525cece7424dd41a8
kauatwn <kaualopescontato@gmail.com>
2024-10-30 15:04:02 -0300

    Refatorando PaymentsController para DocumentsController

---
+++ src/API/Controllers/DocumentsController.cs
@@ -0,0 +1,18 @@
+﻿using Application.Abstractions.UseCases;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DocumentsController : ControllerBase
+{
+    [HttpPost("Print")]
+    [ProducesResponseType<Document>(StatusCodes.Status201Created)]
+    public IActionResult Print(IPrintDocumentUseCase useCase, Document document)
+    {
+        var result = useCase.Execute(document);
+        return Created(string.Empty, result);
+    }
+}
