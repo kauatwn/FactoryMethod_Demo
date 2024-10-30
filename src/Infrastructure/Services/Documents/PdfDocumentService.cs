4525e56971d9017bb67d9388f99f5175e155341d
kauatwn <kaualopescontato@gmail.com>
2024-10-30 14:57:47 -0300

    Criando PdfDocumentService

---
+++ src/Infrastructure/Services/Documents/PdfDocumentService.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using Domain.Interfaces.Services;
+
+namespace Infrastructure.Services.Documents;
+
+public class PdfDocumentService : IDocumentService
+{
+    public string PrintDocument(Document document)
+    {
+        return $"PDF Document: {document.Title}";
+    }
+}
