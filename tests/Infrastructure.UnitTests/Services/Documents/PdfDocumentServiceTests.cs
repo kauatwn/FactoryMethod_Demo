9ceb954429a1cd7902a12ea9da12b9301f284fd4
kauatwn <kaualopescontato@gmail.com>
2024-11-09 19:57:00 -0300

    Criando teste unitário para PdfDocumentService

---
+++ tests/Infrastructure.UnitTests/Services/Documents/PdfDocumentServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Infrastructure.Services.Documents;
+
+namespace Infrastructure.UnitTests.Services.Documents;
+
+public class PdfDocumentServiceTests
+{
+    private PdfDocumentService Service { get; } = new();
+
+    [Fact]
+    public void ShouldPrintPdfDocumentSuccessfully()
+    {
+        // Arrange
+        var document = new Document("Title");
+        var expected = $"PDF Document: {document.Title}";
+
+        // Act
+        var result = Service.Print(document);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
