1e5b472a8d989919a673b0071bf8628ea3a6f905
kauatwn <kaualopescontato@gmail.com>
2024-11-09 19:57:18 -0300

    Criando teste unitário para WordDocumentService

---
+++ tests/Infrastructure.UnitTests/Services/Documents/WordDocumentServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Infrastructure.Services.Documents;
+
+namespace Infrastructure.UnitTests.Services.Documents;
+
+public class WordDocumentServiceTests
+{
+    private WordDocumentService Service { get; } = new();
+
+    [Fact]
+    public void ShouldPrintWordDocumentSuccessfully()
+    {
+        // Arrange
+        var document = new Document("Title");
+        var expected = $"Word Document: {document.Title}";
+
+        // Act
+        var result = Service.Print(document);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
