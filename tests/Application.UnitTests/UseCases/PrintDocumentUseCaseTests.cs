96272343294d16bb89f607069b1aee2bc8947d4f
kauatwn <kaualopescontato@gmail.com>
2024-11-09 19:55:12 -0300

    Renomeando PrintDocumentUseCaseTest e ajuste dos mocks para PrintDocumentUseCase

--- tests/Application.UnitTests/UseCases/PrintDocumentUseCaseTests.cs
+++ tests/Application.UnitTests/UseCases/PrintDocumentUseCaseTests.cs
@@ -6,13 +6,13 @@ using Moq;
 
 namespace Application.UnitTests.UseCases;
 
-public class ProcessPaymentUseCaseTest
+public class PrintDocumentUseCaseTests
 {
     private Mock<IDocumentFactory> DocumentFactoryMock { get; } = new();
     private Mock<IDocumentService> DocumentServiceMock { get; } = new();
     private PrintDocumentUseCase UseCase { get; }
 
-    public ProcessPaymentUseCaseTest()
+    public PrintDocumentUseCaseTests()
     {
         UseCase = new PrintDocumentUseCase(DocumentFactoryMock.Object);
     }
@@ -24,18 +24,15 @@ public class ProcessPaymentUseCaseTest
         var document = new Document("Title");
         const string expected = "Document printed successfully.";
 
-        DocumentFactoryMock.Setup(pf => pf.Create()).Returns(DocumentServiceMock.Object);
-
-        DocumentServiceMock.Setup(ps => ps.PrintDocument(It.IsAny<Document>())).Returns(expected);
+        DocumentFactoryMock.Setup(df => df.Create()).Returns(DocumentServiceMock.Object);
+        DocumentServiceMock.Setup(ds => ds.Print(It.IsAny<Document>())).Returns(expected);
 
         // Act
         var result = UseCase.Execute(document);
 
         // Assert
         Assert.Equal(expected, result);
-
-        DocumentFactoryMock.Verify(pf => pf.Create(), Times.Once);
-
-        DocumentServiceMock.Verify(ps => ps.PrintDocument(It.IsAny<Document>()), Times.Once);
+        DocumentFactoryMock.Verify(df => df.Create(), Times.Once);
+        DocumentServiceMock.Verify(ds => ds.Print(It.IsAny<Document>()), Times.Once);
     }
 }
