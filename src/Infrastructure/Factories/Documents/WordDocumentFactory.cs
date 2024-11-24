ec80ba14403f9d8ba86714d8ad3cd7ee5e87fa9b
kauatwn <kaualopescontato@gmail.com>
2024-11-24 20:16:34 -0300

    Adiciona suporte a configuração dinâmica de watermark no WordDocumentFactory

--- src/Infrastructure/Factories/Documents/WordDocumentFactory.cs
+++ src/Infrastructure/Factories/Documents/WordDocumentFactory.cs
@@ -1,13 +1,18 @@
 ﻿using Domain.Interfaces.Factories;
 using Domain.Interfaces.Services;
 using Infrastructure.Services.Documents;
+using Microsoft.Extensions.Configuration;
 
 namespace Infrastructure.Factories.Documents;
 
-public class WordDocumentFactory : IDocumentFactory
+public class WordDocumentFactory(IConfiguration configuration) : IDocumentFactory
 {
+    private IConfiguration Configuration { get; } = configuration;
+
     public IDocumentService Create()
     {
-        return new WordDocumentService();
+        var watermark = Configuration["DocumentSettings:Watermark"] ?? "Default Watermark";
+
+        return new WordDocumentService(watermark);
     }
 }
