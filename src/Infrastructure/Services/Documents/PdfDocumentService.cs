cc768ae7ed5e822b56e341b9d8936c0c133e0a02
kauatwn <kaualopescontato@gmail.com>
2024-11-24 20:08:07 -0300

    Adiciona suporte a watermark no PdfDocumentService

--- src/Infrastructure/Services/Documents/PdfDocumentService.cs
+++ src/Infrastructure/Services/Documents/PdfDocumentService.cs
@@ -3,10 +3,12 @@ using Domain.Interfaces.Services;
 
 namespace Infrastructure.Services.Documents;
 
-public class PdfDocumentService : IDocumentService
+public class PdfDocumentService(string watermark) : IDocumentService
 {
+    public string Watermark { get; private set; } = watermark;
+
     public string Print(Document document)
     {
-        return $"PDF Document: {document.Title}";
+        return $"PDF Document: {document.Title} - {Watermark}";
     }
 }
