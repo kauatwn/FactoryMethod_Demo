e86949fe70cdab89b324abaa68732e83e887983d
kauatwn <kaualopescontato@gmail.com>
2024-11-24 21:08:35 -0300

    Atualiza teste do WordDocumentService para incluir watermark no construtor

--- tests/Infrastructure.UnitTests/Services/Documents/WordDocumentServiceTests.cs
+++ tests/Infrastructure.UnitTests/Services/Documents/WordDocumentServiceTests.cs
@@ -5,14 +5,14 @@ namespace Infrastructure.UnitTests.Services.Documents;
 
 public class WordDocumentServiceTests
 {
-    private WordDocumentService Service { get; } = new();
+    private WordDocumentService Service { get; } = new("Watermark");
 
     [Fact]
     public void ShouldPrintWordDocumentSuccessfully()
     {
         // Arrange
         var document = new Document("Title");
-        var expected = $"Word Document: {document.Title}";
+        var expected = $"Word Document: {document.Title} - {Service.Watermark}";
 
         // Act
         var result = Service.Print(document);
