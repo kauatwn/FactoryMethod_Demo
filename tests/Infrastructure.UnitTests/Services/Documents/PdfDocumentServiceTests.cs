92949ac26f0c8cc6ab09df15a064428806048b00
kauatwn <kaualopescontato@gmail.com>
2024-11-24 21:08:29 -0300

    Atualiza teste do PdfDocumentService para incluir watermark no construtor

--- tests/Infrastructure.UnitTests/Services/Documents/PdfDocumentServiceTests.cs
+++ tests/Infrastructure.UnitTests/Services/Documents/PdfDocumentServiceTests.cs
@@ -5,14 +5,14 @@ namespace Infrastructure.UnitTests.Services.Documents;
 
 public class PdfDocumentServiceTests
 {
-    private PdfDocumentService Service { get; } = new();
+    private PdfDocumentService Service { get; } = new("Watermark");
 
     [Fact]
     public void ShouldPrintPdfDocumentSuccessfully()
     {
         // Arrange
         var document = new Document("Title");
-        var expected = $"PDF Document: {document.Title}";
+        var expected = $"PDF Document: {document.Title} - {Service.Watermark}";
 
         // Act
         var result = Service.Print(document);
