21b959708505c2626a4703e080540f13e69e4db0
kauatwn <kaualopescontato@gmail.com>
2024-11-30 11:35:59 -0300

    Renomeando PdfDocumentService para PdfReportService e atualizando método para Generate

---
+++ src/Infrastructure/Services/Documents/PdfReportService.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Domain.Interfaces.Services;
+
+namespace Infrastructure.Services.Documents;
+
+public class PdfReportService(string watermark) : IReportService
+{
+    public string Watermark { get; } = watermark;
+
+    public string Generate(Report report)
+    {
+        return $"PDF Report: {report.Title} - {Watermark}\nContent: {report.Content}";
+    }
+}
