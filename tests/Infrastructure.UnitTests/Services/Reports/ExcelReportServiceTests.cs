370dccf7228f833ff9c1907956dc7d2bab2dd235
kauatwn <kaualopescontato@gmail.com>
2024-11-30 16:45:52 -0300

    Renomeando WordDocumentServiceTests para ExcelReportServiceTests

---
+++ tests/Infrastructure.UnitTests/Services/Reports/ExcelReportServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Infrastructure.Services.Documents;
+
+namespace Infrastructure.UnitTests.Services.Reports;
+
+public class ExcelReportServiceTests
+{
+    private ExcelReportService Service { get; } = new("Template");
+
+    [Fact]
+    public void ShouldGenerateExcelReportSuccessfully()
+    {
+        // Arrange
+        var report = new Report("Title", "Content");
+        var expected = $"Excel Report: {report.Title} - Template: {Service.Template}\nContent: {report.Content}";
+
+        // Act
+        string result = Service.Generate(report);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
