1cce0e55498ce8d77b1842a5bfd5340bcb233531
kauatwn <kaualopescontato@gmail.com>
2024-11-30 16:44:54 -0300

    Renomeando PdfDocumentServiceTests para PdfReportServiceTests

---
+++ tests/Infrastructure.UnitTests/Services/Reports/PdfReportServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Infrastructure.Services.Documents;
+
+namespace Infrastructure.UnitTests.Services.Reports;
+
+public class PdfReportServiceTests
+{
+    private PdfReportService Service { get; } = new("Watermark");
+
+    [Fact]
+    public void ShouldGeneratePdfReportSuccessfully()
+    {
+        // Arrange
+        var report = new Report("Title", "Content");
+        var expected = $"PDF Report: {report.Title} - {Service.Watermark}\nContent: {report.Content}";
+
+        // Act
+        string result = Service.Generate(report);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
