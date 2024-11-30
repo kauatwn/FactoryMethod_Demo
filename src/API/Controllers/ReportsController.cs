426ad49b6fb2b8b74c58f8224471f0d686634ff6
kauatwn <kaualopescontato@gmail.com>
2024-11-30 11:56:43 -0300

    Renomeando DocumentsController para ReportsController

---
+++ src/API/Controllers/ReportsController.cs
@@ -0,0 +1,19 @@
+﻿using Application.Abstractions.UseCases;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ReportsController : ControllerBase
+{
+    [HttpPost("Generate")]
+    [ProducesResponseType<Report>(StatusCodes.Status200OK)]
+    public IActionResult Generate(IGenerateReportUseCase useCase, Report document)
+    {
+        string result = useCase.Execute(document);
+
+        return Ok(result);
+    }
+}
