3ad1327e2253e0b299103a10d0866e9e73358be5
kauatwn <kaualopescontato@gmail.com>
2024-11-30 11:23:28 -0300

    Renomeando Document para Report e adicionando propriedade Content

--- src/Domain/Entities/Document.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Domain.Entities;
-
-public class Document(string title)
-{
-
-    public string Title { get; set; } = title;
-}
---
+++ src/Domain/Entities/Report.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Entities;
+
+public class Report(string title, string content)
+{
+    public string Title { get; set; } = title;
+    public string Content { get; set; } = content;
+}
