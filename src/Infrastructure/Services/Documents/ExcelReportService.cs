dd01c7f42d2c719194b1eff84349f84b73465173
kauatwn <kaualopescontato@gmail.com>
2024-11-30 11:37:51 -0300

    Renomeando WordDocumentService para ExcelReportService e atualizando método para Generate

---
+++ src/Infrastructure/Services/Documents/ExcelReportService.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Domain.Interfaces.Services;
+
+namespace Infrastructure.Services.Documents;
+
+public class ExcelReportService(string template) : IReportService
+{
+    public string Template { get; } = template;
+
+    public string Generate(Report report)
+    {
+        return $"Excel Report: {report.Title} - Template: {Template}\nContent: {report.Content}";
+    }
+}
