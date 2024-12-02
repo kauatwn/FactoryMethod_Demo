31a96a0967474c759117997cdb41ce009b73c699
kauatwn <kaualopescontato@gmail.com>
2024-12-02 19:24:13 -0300

    Refatorando teste unitário da PdfReportFactory para usar IOptions

--- tests/Infrastructure.UnitTests/Factories/Reports/PdfReportFactoryTests.cs
+++ tests/Infrastructure.UnitTests/Factories/Reports/PdfReportFactoryTests.cs
@@ -1,27 +1,30 @@
 using Domain.Interfaces.Services;
-using Infrastructure.Factories.Documents;
-using Infrastructure.Services.Documents;
-using Microsoft.Extensions.Configuration;
+using Infrastructure.Factories.Reports;
+using Infrastructure.Options;
+using Infrastructure.Services.Reports;
+using Microsoft.Extensions.Options;
 using Moq;
 
 namespace Infrastructure.UnitTests.Factories.Reports;
 
 public class PdfReportFactoryTests
 {
-    private Mock<IConfiguration> MockConfiguration { get; } = new();
+    private Mock<IOptions<ReportOptions>> MockOptions { get; } = new();
     private PdfReportFactory Factory { get; }
 
     public PdfReportFactoryTests()
     {
-        Factory = new PdfReportFactory(MockConfiguration.Object);
+        Factory = new PdfReportFactory(MockOptions.Object);
     }
 
     [Fact]
-    public void ShouldCreatePdfReportServiceWithWatermark()
+    public void ShouldCreatePdfReportServiceWithWatermarkWhenIsProvided()
     {
         // Arrange
-        const string expected = "Original PDF Watermark";
-        MockConfiguration.Setup(c => c["ReportSettings:PdfWatermark"]).Returns(expected);
+        var options = new ReportOptions { PdfWatermark = "Custom PDF Watermark" };
+        const string expected = "Custom PDF Watermark";
+
+        MockOptions.Setup(o => o.Value).Returns(options);
 
         // Act
         IReportService service = Factory.Create();
@@ -29,16 +32,18 @@ public class PdfReportFactoryTests
         // Assert
         var pdfReportService = Assert.IsType<PdfReportService>(service);
         Assert.Equal(expected, pdfReportService.Watermark);
-        
-        MockConfiguration.Verify(c => c["ReportSettings:PdfWatermark"], Times.Once);
+
+        MockOptions.Verify(o => o.Value, Times.Once);
     }
 
     [Fact]
-    public void ShouldCreatePdfReportServiceWithDefaultWatermarkWhenNotProvided()
+    public void ShouldCreatePdfReportServiceWithDefaultWatermarkWhenIsNotProvided()
     {
         // Arrange
+        var options = new ReportOptions();
         const string expected = "Default PDF Watermark";
-        MockConfiguration.Setup(c => c["ReportSettings:PdfWatermark"]).Returns((string?)null);
+
+        MockOptions.Setup(o => o.Value).Returns(options);
 
         // Act
         IReportService service = Factory.Create();
@@ -46,7 +51,7 @@ public class PdfReportFactoryTests
         // Assert
         var pdfReportService = Assert.IsType<PdfReportService>(service);
         Assert.Equal(expected, pdfReportService.Watermark);
-        
-        MockConfiguration.Verify(c => c["ReportSettings:PdfWatermark"], Times.Once);
+
+        MockOptions.Verify(o => o.Value, Times.Once);
     }
 }
