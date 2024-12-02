dfad130a52d528c2af8386cc713de45c756bfbad
kauatwn <kaualopescontato@gmail.com>
2024-12-02 19:19:43 -0300

    Refatorar ExcelReportFactory para usar IOptions e novo namespace

---
+++ src/Infrastructure/Factories/Reports/ExcelReportFactory.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Interfaces.Factories;
+using Domain.Interfaces.Services;
+using Infrastructure.Options;
+using Infrastructure.Services.Reports;
+using Microsoft.Extensions.Options;
+
+namespace Infrastructure.Factories.Reports;
+
+public class ExcelReportFactory(IOptions<ReportOptions> options) : IReportFactory
+{
+    private IOptions<ReportOptions> Options { get; } = options;
+
+    public IReportService Create()
+    {
+        string template = Options.Value.ExcelTemplate ?? "Default Excel Template";
+
+        return new ExcelReportService(template);
+    }
+}
