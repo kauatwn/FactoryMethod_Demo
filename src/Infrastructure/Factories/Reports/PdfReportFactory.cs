7afb6394eb96c33a9fd5c27d1cc257c12e9baacb
kauatwn <kaualopescontato@gmail.com>
2024-12-02 19:19:56 -0300

    Refatorar PdfReportFactory para usar IOptions e novo namespace

---
+++ src/Infrastructure/Factories/Reports/PdfReportFactory.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Interfaces.Factories;
+using Domain.Interfaces.Services;
+using Infrastructure.Options;
+using Infrastructure.Services.Reports;
+using Microsoft.Extensions.Options;
+
+namespace Infrastructure.Factories.Reports;
+
+public class PdfReportFactory(IOptions<ReportOptions> options) : IReportFactory
+{
+    private IOptions<ReportOptions> Options { get; } = options;
+
+    public IReportService Create()
+    {
+        string watermark = Options.Value.PdfWatermark ?? "Default PDF Watermark";
+
+        return new PdfReportService(watermark);
+    }
+}
