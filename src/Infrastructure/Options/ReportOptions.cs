ad342fbde7524bc07138138b654167f0455ca09e
kauatwn <kaualopescontato@gmail.com>
2024-12-02 19:18:59 -0300

    Criar ReportOptions para configuração de ReportSettings

---
+++ src/Infrastructure/Options/ReportOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Infrastructure.Options;
+
+public class ReportOptions
+{
+    public const string Key = "ReportSettings";
+
+    public string? PdfWatermark { get; init; }
+    public string? ExcelTemplate { get; init; }
+}
